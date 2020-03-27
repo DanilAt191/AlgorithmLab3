badb576464d7e5f8d551c21c7b5b47da9b0d8ee6
DanilAt191 <61827117+DanilAt191@users.noreply.github.com>
2020-03-27 11:33:43 +0200

    Add files via upload

---
+++ AlgorithmLab3(midle).cpp
@@ -0,0 +1,24 @@
+﻿#include <cctype>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	//Средний уровень, 3 вариант: дан массив символов содержащий текст. Определить длину содержащийся в нем максимальной серии символов отличных от букв
+	setlocale(LC_ALL, "");
+	int maxlen = 0;
+	int curlen = 0;
+	const int N = 15;
+	char ch;
+	for (int i = 0; i < N;i++) 
+	{
+		cin >> ch;
+		if (isalpha(ch) == 0)
+			curlen = curlen + 1;
+		maxlen = max(curlen, maxlen);
+	}
+	cout <<"Максимальная серия: "<< maxlen << endl;
+	return 0;
+}
